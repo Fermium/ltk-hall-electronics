14d3e48c946cf17c39f7d40334cf915a245ce371
Davide Bortolami <d@fermiumlabs.com>
2017-09-01 17:30:47 +0200

    new backup script

--- scripts/backup-all.sh
+++ scripts/backup-all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #s3cmd put --recursive ../* "s3://altium-backup/hall-electronic/whole/$(git rev-parse --short HEAD)/"
-gsutil -m cp -r ../* "gs://altium-backup/hall-electronic/whole/$(git rev-parse --short HEAD)/"
+HASH=$(git rev-parse --short HEAD)
+TARGZ_NAME="/tmp/$HASH.tar.gz"
+mkdir "/tmp/$HASH"
+tar -zcvf "$TARGZ_NAME" ../
+#gsutil -m cp  "$TARGZ_NAME" "gs://altium-backup/hall-electronic/whole/"
+s3cmd put "$TARGZ_NAME" "s3://altium-backup/hall-electronic/whole/"
+rm "$TARGZ_NAME"
