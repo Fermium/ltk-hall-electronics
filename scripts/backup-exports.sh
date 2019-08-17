74b03481b2027bdcbc17450364dbf5cf89d79b44
ddavidebor <d@fermiumlabs.com>
2019-08-17 18:54:23 +0200

    no message

--- scripts/backup-exports.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#s3cmd put --recursive ../exports/* "s3://altium-backup/hall-electronic/exports/$(git rev-parse --short HEAD)/"
-gsutil -m cp -r ../exports/ "gs://altium-backup/hall-electronic/exports/$(git rev-parse --short HEAD)"
-
-echo "Exported on Google cloud storage with hash $(git rev-parse --short HEAD)"
